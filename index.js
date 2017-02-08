4b6b1dd1bff10a2527d364e9bbb68d083db86ff2
Plamen Andonov <pankooo@gmail.com>
2017-02-08 17:21:18 +0000

    Heroku port fix

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ var app = express();
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-io.configure(function () {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.render('index.html');
@@ -294,6 +287,6 @@ io.on('connection', function(socket){
   });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.listen(app.get('port'), function(){
+  console.log('listening on '+app.get('port'));
 });
